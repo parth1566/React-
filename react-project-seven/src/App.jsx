cf69a84b3687a9d0fdfda18c0f3c05b459a0f2ce
Parth Sharma <parthsh.161106@gmail.com>
2026-07-23 14:20:52 +0530

    'ReactBasics'

---
+++ react-project-seven/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  function handleClick() {
+    alert("I am clicked");
+  }
+
+  function handleMouseOver() {
+    alert("Welcome to para")
+  }
+  
+  function handleInputChange(e) {
+    //console.log("Value changed in Input")
+    console.log("Value till now", e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Confirm to submit form ?")
+  }
+
+  return (
+    <div>
+
+      {/* <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInputChange}/>
+        <button type='submit'>Submit</button>
+      </form> */}
+    <p onMouseOver={handleMouseOver} style={{border : "1px solid blue"}}> 
+      I am a Para
+    </p>
+
+    <button onClick={handleClick}>
+      Click Me
+    </button>
+    </div>
+  )
+}
+
+export default App
