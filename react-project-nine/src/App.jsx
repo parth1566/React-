8bd06724fd6165cf9acb1eaa9d1d3091320d9c84
Parth Sharma <parthsh.161106@gmail.com>
2026-07-24 16:04:59 +0530

    ReactBasics

--- react-project-nine/src/App.jsx
+++ react-project-nine/src/App.jsx
@@ -1,23 +1,37 @@
 import { createContext, useState } from 'react'
 import ChildA from './components/ChildA';
+import './App.css'
 
 // Step 1: create context
-const UserContext = createContext();
+//const UserContext = createContext();
 // Step 2: wrap all the child inside a provider
 // Step 3: pass value
 // Step 4: consume the data inside consumer
 
-function App() {
-  const[user, setUser] = useState({name: "Parth"});
 
+const ThemeContext = createContext();
+
+function App() {
+  // const[user, setUser] = useState({name: "Parth"});
+  const[theme, setTheme] = useState('light');
   return (
-    <div>
-      <UserContext.Provider value={user}>
-        <ChildA/>
-      </UserContext.Provider>
-    </div>
+      <ThemeContext.Provider value={{theme, setTheme}}>
+        <div id='container' style={{backgroundColor: theme === 'light' ? 'beige' : 'black'}}>
+           <ChildA/>
+        </div>
+      </ThemeContext.Provider>
+
+
+
+
+    // <div>
+    //   <UserContext.Provider value={user}>
+    //     <ChildA/>
+    //   </UserContext.Provider>
+    // </div>
   )
 }
 
 export default App
-export {UserContext}
+//export {UserContext}
+export {ThemeContext}
