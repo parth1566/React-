b7d75a7520bb67d4875c64bd5abf7404fb2c51a6
Parth Sharma <parthsh.161106@gmail.com>
2026-07-31 23:46:55 +0530

    ReactBasics'

---
+++ react-form-project/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { useForm  } from "react-hook-form"
+import './App.css'
+
+
+function App() {
+
+   const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  
+  async function onSubmit(data) {
+    // API call ko simulate krte h
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("submitting the form", data);
+  }
+
+  return (
+    <>
+      <form  onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>First Name: </label>
+          <input
+           className={errors.firstName ? 'input-error' : ""}
+           {...register('firstName', 
+            {
+              required: true, 
+              minLength: {value: 3, message: 'Min Len atleast 3'}, 
+              maxLength: {value: 6, message: "Max Len atmost 6"}
+            })}/>
+            {errors.firstName && <p className='error-msg'>{errors.firstName.message}</p>}
+            {}
+        </div>
+        <br />
+        <div>
+          <label>Middle Name: </label>
+          <input 
+          className={errors.firstName ? 'input-error' : ""}
+          {...register('middleName')}/>
+        </div>
+        <br />
+        <div>
+          <label>Last Name: </label>
+          <input 
+          className={errors.firstName ? 'input-error' : ""}
+          {...register('lastName',{
+            pattern: {
+              value: /^[A-Za-z]+$/i ,
+              message: 'Last Name is not as per the rules'
+            }
+          })}/>
+          {errors.lastName && <p className='error-msg'>{errors.lastName.message}</p>}
+        </div>
+        <br />
+       <button disabled={isSubmitting}>
+    {isSubmitting ? "Submitting..." : "Submit"}
+       </button>
+      </form>
+    </>
+  )
+}
+
+export default App
